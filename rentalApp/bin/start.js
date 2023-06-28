1375b9ae7a23319225c12191443bb0af74d64057
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-06-28 10:54:33 +0300

    changed some few staffs

--- rentalApp/bin/start.js
+++ rentalApp/bin/start.js
@@ -1,4 +1,116 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
+
+// /**
+//  * Module dependencies.
+//  */
+
+// import app from "../app.js";
+// import createDebugger from "debug";
+// import http from "http";
+// import mongoose from "mongoose";
+// import dotenv from 'dotenv'
+
+// dotenv.config();
+// const debug = createDebugger('rental-app:server')
+
+// async function connectToMongoDB() {
+//   try {
+//     mongoose.set("strictQuery", false);
+//     await mongoose.connect(process.env.MONGO_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     console.log("Connected to MongoDB");
+//     startServer();
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     process.exit(1);
+//   }
+// }
+
+// /**
+//  * Get port from environment and store in Express.
+//  */
+
+
+
+// function startServer() {
+//   /**
+//    * Get port from environment and store in Express.
+//    */
+//   const port = normalizePort(process.env.PORT || "3000");
+//   app.set("port", port);
+
+//   /**
+//    * Create HTTP server.
+//    */
+//   const server = http.createServer(app);
+
+//   /**
+//    * Listen on provided port, on all network interfaces.
+//    */
+//   server.listen(port);
+//   server.on("error", onError);
+//   server.on("listening", onListening);
+
+//   /**
+//    * Normalize a port into a number, string, or false.
+//    */
+//   function normalizePort(val) {
+//     const port = parseInt(val, 10);
+
+//     if (isNaN(port)) {
+//       // named pipe
+//       return val;
+//     }
+
+//     if (port >= 0) {
+//       // port number
+//       return port;
+//     }
+
+//     return false;
+//   }
+
+//   /**
+//    * Event listener for HTTP server "error" event.
+//    */
+//   function onError(error) {
+//     if (error.syscall !== "listen") {
+//       throw error;
+//     }
+
+//     const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+//     // handle specific listen errors with friendly messages
+//     switch (error.code) {
+//       case "EACCES":
+//         console.error(bind + " requires elevated privileges");
+//         process.exit(1);
+//         break;
+//       case "EADDRINUSE":
+//         console.error(bind + " is already in use");
+//         process.exit(1);
+//         break;
+//       default:
+//         throw error;
+//     }
+//   }
+
+//   /**
+//    * Event listener for HTTP server "listening" event.
+//    */
+//   function onListening() {
+//     const addr = server.address();
+//     const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+//     debug("Listening on " + bind);
+//   }
+// }
+
+// // Call the connectToMongoDB function to connect to MongoDB
+// connectToMongoDB();
+
+// #!/usr/bin/env node
 
 /**
  * Module dependencies.
@@ -7,131 +119,102 @@
 import app from "../app.js";
 import createDebugger from "debug";
 import http from "http";
-import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import { MongoClient } from "mongodb";
 
-dotenv.config();
-const debug = createDebugger('rental-app:server')
-
-async function connectToMongoDB() {
-  try {
-    mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log("Connected to MongoDB");
-    startServer();
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
-  }
-}
+const debug = createDebugger('rental-app:server');
 
 /**
  * Get port from environment and store in Express.
  */
 
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
+/**
+ * Create HTTP server.
+ */
 
-function startServer() {
-  /**
-   * Get port from environment and store in Express.
-   */
-  const port = normalizePort(process.env.PORT || "3000");
-  app.set("port", port);
+const server = http.createServer(app);
 
-  /**
-   * Create HTTP server.
-   */
-  const server = http.createServer(app);
+// MongoDB connection URI
+const mongoURI = "mongodb://mongodb-service:27017/rental-app"; // Update with your MongoDB connection details
 
-  /**
-   * Listen on provided port, on all network interfaces.
-   */
-  server.listen(port);
-  server.on("error", onError);
-  server.on("listening", onListening);
+/**
+ * Connect to MongoDB before starting the server.
+ */
+MongoClient.connect(mongoURI, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to MongoDB");
+    const db = client.db(); // Specify your database name if it's different from the one in the connection URI
+    app.set("db", db); // Set the MongoDB client instance in the app
 
-  /**
-   * Normalize a port into a number, string, or false.
-   */
-  function normalizePort(val) {
-    const port = parseInt(val, 10);
+    /**
+     * Listen on provided port, on all network interfaces.
+     */
 
-    if (isNaN(port)) {
-      // named pipe
-      return val;
-    }
+    server.listen(port);
+    server.on("error", onError);
+    server.on("listening", onListening);
 
-    if (port >= 0) {
-      // port number
-      return port;
-    }
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
+    process.exit(1);
+  });
 
-    return false;
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
   }
 
-  /**
-   * Event listener for HTTP server "error" event.
-   */
-  function onError(error) {
-    if (error.syscall !== "listen") {
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
       throw error;
-    }
-
-    const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-      case "EACCES":
-        console.error(bind + " requires elevated privileges");
-        process.exit(1);
-        break;
-      case "EADDRINUSE":
-        console.error(bind + " is already in use");
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  }
-
-  /**
-   * Event listener for HTTP server "listening" event.
-   */
-  function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-    debug("Listening on " + bind);
   }
 }
 
-// Call the connectToMongoDB function to connect to MongoDB
-connectToMongoDB();
+/**
+ * Event listener for HTTP server "listening" event.
+ */
 
-// // MongoDB connection URI
-// const mongoURI = "mongodb://mongo-service:27017/your-database-name"; // Update with your MongoDB connection details
-
-// /**
-//  * Connect to MongoDB before starting the server.
-//  */
-// MongoClient.connect(mongoURI, { useUnifiedTopology: true })
-//   .then((client) => {
-//     console.log("Connected to MongoDB");
-//     const db = client.db(); // Specify your database name if it's different from the one in the connection URI
-//     app.set("db", db); // Set the MongoDB client instance in the app
-
-//     /**
-//      * Listen on provided port, on all network interfaces.
-//      */
-
-//     server.listen(port);
-//     server.on("error", onError);
-//     server.on("listening", onListening);
-
-//   })
-//   .catch((error) => {
-//     console.error("Failed to connect to MongoDB:", error);
-//     process.exit(1);
-//   });
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
+;
