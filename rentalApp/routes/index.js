f67cde22e15ab2a82731d7ffb6bda5d051c566e5
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-10-21 05:13:03 +0300

    added some stafs

--- rentalApp/routes/index.js
+++ rentalApp/routes/index.js
@@ -29,6 +29,6 @@ router.route('/users/').get(getAllUsers);
 router.route('/reset-password/:token').post(UpdatePwd);
 router.route('/pwd-reset-emails/').post(pwdResetEmails);
 router.route('/users/:id').put(AuthenticateToken,UpdateUser).get(getSingleUser).delete(deleteUser);
-router.route('/properties').post(upload.fields('photos'),addProperties).get(GetAllProperties);
+router.route('/properties').post(upload.fields([{ name: 'photos' }]),addProperties).get(GetAllProperties);
 router.route('/property/:id').get(GetSingleProperty).delete(deleteProperty).put(UpdateProperty);
 export default router;
