5a41574c6dc0e77850b83bde9ddbc6569a2550b5
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-11-14 18:30:30 +0300

    check something

--- rentalApp/Controllers/Auth/VerifyCode.js
+++ rentalApp/Controllers/Auth/VerifyCode.js
@@ -1,5 +1,5 @@
 import { Owner } from "../../Models/Owner.js";
-
+import crypto from "crypto";
 const generateAuthToken = (userId, email,name) => {
   return jwt.sign({ userId: userId, email: email,name:name }, process.env.JWT_SECRET, {
     expiresIn: "1h",
@@ -7,8 +7,11 @@ const generateAuthToken = (userId, email,name) => {
 };
 export const verifyCode = async (req, res) => {
   try {
+    const algorithm = 'aes-256-cbc';
+  const EncryptionKey = process.env.EncryptionKey;
+  const InitializationVector = '6d0cf9de18a8c78b5f888b42d9855bd2';
     const encryptedData = req.body.data;
-    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(yourEncryptionKey), Buffer.from(yourInitializationVector));
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(EncryptionKey), Buffer.from(InitializationVector));
     let decrypted = decipher.update(encryptedData, 'hex', 'utf-8');
     decrypted += decipher.final('utf-8');
     
