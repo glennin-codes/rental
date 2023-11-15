14e78c0fa1a58407bbd0372429a1103ef9b087de
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-11-15 17:40:06 +0300

    fixed issue

--- rentalApp/Controllers/Auth/requestVerification.js
+++ rentalApp/Controllers/Auth/requestVerification.js
@@ -1,17 +1,35 @@
+import { Owner } from "../../Models/Owner.js";
 import { VerifyEmail } from "../Util/Email/EmailSender.js";
 
-export const requestVerification=(req,res)=>{
+export const requestVerification=async(req,res)=>{
     
     const {email,name}=req.body
     console.log(email,name);
     const verificationCode=Math.floor(Math.random() * 900000) + 100000;
-    const verify={
-        email:email,
-        name:name,
-        code:verificationCode,
-
+   
+    try {
+        const user=await Owner.findOne({email});
+        if(!user){
+            return res.status(404).json({error:"user not found"})
+        }
+        if(user.isVerified){
+            return res.status(400).json({error:"user is already verified"})
+        }
+        user.verificationCode =verificationCode;
+        await user.save();
+        const verify={
+            email,
+            name:user.name,
+            code:user.verificationCode,
+    
+        }
+        VerifyEmail(verify);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error:"server error"})
+        
     }
-    VerifyEmail(verify);
+   
     res.status(200).json({message:"verification code was sent to your email"})
    
 
